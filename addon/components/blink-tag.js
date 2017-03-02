adfbf9762f7c612f0157f88b9e3f2b6218a2563c
Alan Dong <adong@yahoo-inc.com>
2017-03-01 16:29:19 -0800

    Using ES6 Syntax

--- addon/components/blink-tag.js
+++ addon/components/blink-tag.js
@@ -4,11 +4,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'h1',
   show: true,
-  classNameBindings: ['show:ember:blink-tag--invisible'],
+  classNameBindings: ['show:blink-tag--visible:blink-tag--invisible'],
   didInsertElement: function() {
-    var self = this;
-    setInterval(function() {
-      self.toggleProperty('show');
-    }, 300);
+    setInterval(() => {
+      this.toggleProperty('show');
+    }, 3000);
   }
 });
